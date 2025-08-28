f0b9237de8df2301934e289ad61a7e8b7dba993c
Anas Ghouila <ghouilaa.anas@gmail.com>
2025-08-28 10:22:24 +0100

    Create Gateway.ino

---
+++ Source code/Gateway/Gateway.ino
@@ -0,0 +1,18 @@
+/*
+  This source code file belongs to Swatekk.
+  It is the intellectual property of the company and is intended only for demonstration purposes.
+
+  The gateway is designed to communicate with the device as part of the IoT system.
+  For interested parties, the demo video (version 0) explains how the gateway and device operate,
+  as well as how communication between them is established.
+
+  Note: This file only contains the structure of the program without implementation.
+*/
+
+void setup() {
+  // Initialization code for the gateway would go here
+}
+
+void loop() {
+  // Main program logic for the gateway would go here
+}
